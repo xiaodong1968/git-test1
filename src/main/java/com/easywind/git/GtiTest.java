835a11ecfcee7f175cd8550d52a42a52c6e20eb8
YXDong <15037818317@163.com>
2024-03-17 14:49:58 +0800

    第二次提交

--- src/main/java/com/easywind/git/GtiTest.java
+++ src/main/java/com/easywind/git/GtiTest.java
@@ -3,5 +3,6 @@ package com.easywind.git;
 public class GtiTest {
     public static void main(String[] args) {
         System.out.println("\"hello git\" = " + "hello git");
+        System.out.println("\"hello world\" = " + "hello world");
     }
 }
